e09cf0be56d6f05383e4cd8b014ae357d57fec8c
Abdullah AlGhamdi <50756573+AbdullahMu@users.noreply.github.com>
2020-09-13 04:30:01 +0300

    first commit

---
+++ intro_and_foundations/cmake-build-debug/CMakeFiles/intro_and_foundations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intro_and_foundations.dir/main.cpp.obj"
+  "intro_and_foundations.exe"
+  "intro_and_foundations.exe.manifest"
+  "intro_and_foundations.pdb"
+  "libintro_and_foundations.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intro_and_foundations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
